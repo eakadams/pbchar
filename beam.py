9c6003241da72c6b9b073c16535604e8e0b5b85f
Betsey Adams <eakadams@gmail.com>
2020-10-12 14:10:56 +0200

    Write find_sources

--- beam.py
+++ beam.py
@@ -480,7 +480,7 @@ class Beam(object):
         Write them to a temp file in working directory
         """
         bdsf.process_image(self.pbsmfits,adaptive_rms_box=True,
-                           thresh_isl=3.0, thresh_pix=5.0).write_catalog(self.bdsf_output,
+                           thresh_isl=3.0, thresh_pix=5.0).write_catalog(outfile=self.bdsf_output,
                                                                          format='ascii',
                                                                          clobber=True)
         
