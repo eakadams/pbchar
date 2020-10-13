51c3fb9afcb1e8c201c93b9ddf63dcdf58a4c3f8
Betsey Adams <eakadams@gmail.com>
2020-10-13 19:54:09 +0200

    Fix typo double-use of beam

--- run_beams.py
+++ run_beams.py
@@ -12,6 +12,6 @@ cont_obs = ascii.read('dr_year1_cont.csv')
 #to start with, only do first ten as test
 cont_obs = cont_obs[0:10]
 
-for beam,taskid in cont_obs['Beam','ObsID']:
-    B = beam.Beam(taskid,beam,masklevel=0.1)
+for bm,taskid in cont_obs['Beam','ObsID']:
+    B = beam.Beam(taskid,bm,masklevel=0.1)
     B.go()
