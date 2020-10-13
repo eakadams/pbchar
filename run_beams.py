956b924291a425079c04357e957e701a51a2fb08
Betsey Adams <eakadams@gmail.com>
2020-10-13 19:51:25 +0200

    Write wrapper code to run multiple beams

---
+++ run_beams.py
@@ -0,0 +1,17 @@
+#python code to run beam in bulk
+
+from __future__ import print_function()
+
+import beam as beam
+from astropy.io import ascii
+
+#get taskid/beam combination
+#read in data to be released
+cont_obs = ascii.read('dr_year1_cont.csv')
+
+#to start with, only do first ten as test
+cont_obs = cont_obs[0:10]
+
+for beam,taskid in cont_obs['Beam','ObsID']:
+    B = beam.Beam(taskid,beam,masklevel=0.1)
+    B.go()
