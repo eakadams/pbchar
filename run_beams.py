b8486ee279b1904abba5687556c24b500da307b6
Betsey Adams <eakadams@gmail.com>
2020-10-13 20:20:34 +0200

    Run all beams, brute force

--- run_beams.py
+++ run_beams.py
@@ -4,14 +4,21 @@ from __future__ import print_function
 
 import beam as beam
 from astropy.io import ascii
+from multiprocessing import Pool
 
 #get taskid/beam combination
 #read in data to be released
 cont_obs = ascii.read('dr_year1_cont.csv')
 
-#to start with, only do first ten as test
-cont_obs = cont_obs[0:10]
+#test on just ten  entries
+#cont_obs = cont_obs[0:10]
 
+#try to setup parallelization
+#in order to do this
+#skipping parallelization for now
+#want to figure out / learn how to do
+#but I think I can brute force for tonight
+#and do this when fresh
 for bm,taskid in cont_obs['Beam','ObsID']:
     B = beam.Beam(taskid,bm,masklevel=0.1)
     B.go()
