82ec843fb6c0cc4d11c1364a731729b0190d3631
eakadams <37330078+eakadams@users.noreply.github.com>
2020-10-20 12:51:05 +0200

    update test

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import beam as beam
 
 print("Load Beam object; 190807041, 1; mask 10%")
-B = beam.Beam(190807041,1,masklevel=0.1,workingdir='tmp')
+B = beam.Beam(190807041,1,masklevel=0.1,workingdir='tmp', outputdir='test')
 
 #print("Load Beam object; 190823041, 9; mask 10%")
 #B = beam.Beam(190823041,9,masklevel=0.1,workingdir='tmp')
@@ -14,17 +14,17 @@ B = beam.Beam(190807041,1,masklevel=0.1,workingdir='tmp')
 #B = beam.Beam(190823041,9,masklevel=0.1,workingdir='tmp',
 #              pbname='gpall',pbdir='/data/kutkin/cbeams')
 
-print("Get cont image")
-B.get_cont_image()
+#print("Get cont image")
+#B.get_cont_image()
 
-print("Smooth cont image")
-B.convolve()
+#print("Smooth cont image")
+#B.convolve()
 
-print("Get and regrid PB")
-B.regrid_pb()
+#print("Get and regrid PB")
+#B.regrid_pb()
 
-print("Do PB correction")
-B.do_pb()
+#print("Do PB correction")
+#B.do_pb()
 
 B.go()
 
