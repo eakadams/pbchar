5ee10cac8f8cb50a6e30c17465ae9792c7e69945
eakadams <37330078+eakadams@users.noreply.github.com>
2020-10-20 12:41:04 +0200

    update test.py

--- test.py
+++ test.py
@@ -7,12 +7,12 @@ import beam as beam
 print("Load Beam object; 190807041, 1; mask 10%")
 B = beam.Beam(190807041,1,masklevel=0.1,workingdir='tmp')
 
-print("Load Beam object; 190823041, 9; mask 10%")
-B = beam.Beam(190823041,9,masklevel=0.1,workingdir='tmp')
+#print("Load Beam object; 190823041, 9; mask 10%")
+#B = beam.Beam(190823041,9,masklevel=0.1,workingdir='tmp')
 
-print("Load Beam object; 190823041, 9; mask 10%")
-B = beam.Beam(190823041,9,masklevel=0.1,workingdir='tmp',
-              pbname='gpall',pbdir='/data/kutkin/cbeams')
+#print("Load Beam object; 190823041, 9; mask 10%")
+#B = beam.Beam(190823041,9,masklevel=0.1,workingdir='tmp',
+#              pbname='gpall',pbdir='/data/kutkin/cbeams')
 
 print("Get cont image")
 B.get_cont_image()
@@ -25,5 +25,7 @@ B.regrid_pb()
 
 print("Do PB correction")
 B.do_pb()
+
+B.go()
 
 print("Test done")
