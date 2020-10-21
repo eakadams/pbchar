35fd7fda5fe1996e02b95d07f9659b771940a1c0
Betsey Adams <eakadams@gmail.com>
2020-10-21 09:54:55 +0200

    Fix bugs in run_beam update

--- run_beams.py
+++ run_beams.py
@@ -117,7 +117,8 @@ def work():
         for bm,tid in cont_obs['Beam','ObsID']:
             print("Checking before mapping")
             #first check if output exists
-            beam_outputdir = os.path.join(args.outputdir,pbname,"{}:02d".format(bm))
+            beam = "{}:02d".format(bm)
+            beam_outputdir = os.path.join(args.outputdir,pbname,beam)
             match_output = os.path.join(beam_outputdir,
                                         "{0}_matches.csv".format(tid))
             if not os.path.exists(match_output):
